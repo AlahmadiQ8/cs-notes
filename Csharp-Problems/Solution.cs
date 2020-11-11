edc793fd9684c59411eaa0f5e5922f88b1f197f2
Mohammad Mohammad <m91.alahmadi@gmail.com>
2020-11-10 20:33:36 -0500

    boo

--- Csharp-Problems/Solution.cs
+++ Csharp-Problems/Solution.cs
@@ -1,7 +1,9 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Collections.Immutable;
 using System.Linq;
+using System.Reflection.Metadata.Ecma335;
 using System.Threading;
 using FluentAssertions;
 
@@ -9,14 +11,32 @@ namespace Algorithms
 {
     public class Solution : AbstractSolution
     {
-        public int[][] Merge(int[][] intervals) {
+        public ListNode SwapPairs(ListNode head)
+        {
+            if (head == null) return head;
 
+            var dummy = new ListNode(-1);
+            dummy.next = head;
+            var prev = dummy;
+
+            while (head != null && head.next != null)
+            {
+                var first = head;
+                var second = head.next;
+
+                prev.next = second;
+                first.next = second.next;
+                second.next = first;
+
+                prev = first;
+                head = first.next;
+            }
+
+            return dummy.next;
         }
 
         public override void Test()
         {
-            var r = new List<int>();
-            "r.last".sub
         }
     }
 }
--- Java-Problems/src/com/company/Solution.java
+++ Java-Problems/src/com/company/Solution.java
@@ -4,36 +4,41 @@ import java.security.KeyPair;
 import java.util.*;
 
 public class Solution extends AbstractSolution {
+    class Pair {
+        int list_index;
+        int cur_index;
+
+        public Pair(int list_index, int cur_index) {
+            this.list_index = list_index;
+            this.cur_index = cur_index;
+        }
+    }
 
     public int[] smallestRange(List<List<Integer>> nums) {
-        PriorityQueue<LinkedList<Integer>> heapMin = new PriorityQueue<>(nums.size(), Comparator.comparing(LinkedList::getFirst));
+        PriorityQueue<Pair> heapMin = new PriorityQueue<>(nums.size(), Comparator.comparingInt(a -> nums.get(a.list_index).get(a.cur_index)));
 
         int min = Integer.MAX_VALUE;
         int max = Integer.MIN_VALUE;
         int diff = Integer.MAX_VALUE;
-        for (List<Integer> n : nums) {
-            var list = new LinkedList<>(n);
-            heapMin.add(list);
-            if (list.getFirst() > max) {
-                max = list.getFirst();
-            }
+        for (var i = 0; i < nums.size(); i++) {
+            heapMin.add(new Pair(i, 0));
+            max = Math.max(max, nums.get(i).get(0));
         }
 
-        int lastMax = max;
-        while (!heapMin.peek().isEmpty()) {
-            int curMin = heapMin.peek().getFirst();
-            if (lastMax - curMin < diff) {
-                min = curMin;
-                max = lastMax;
+        int last_max = max;
+        while (nums.get(heapMin.peek().list_index).get(heapMin.peek().cur_index) < nums.get(heapMin.peek().list_index).size()) {
+            Pair curMin = heapMin.remove();
+            int value = nums.get(curMin.list_index).get(curMin.cur_index);
+            if (last_max - value < diff) {
+                min = value;
+                max = last_max;
                 diff = max - min;
             }
+            var next = new Pair(curMin.list_index, curMin.cur_index + 1);
+            var next_value = nums.get(next.list_index).get(next.cur_index);
 
-            var removed = heapMin.remove();
-            removed.removeFirst();
-            if (removed.isEmpty()) break;
-
-            if (removed.getFirst() > lastMax) lastMax = removed.getFirst();
-            heapMin.add(removed);
+            if (next_value > last_max) last_max = next_value;
+            heapMin.add(next);
         }
         return new int[]{min, max};
     }
